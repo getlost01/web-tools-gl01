86b5f147e11a9fb2111c3a081a23751c027e2627
Aagam Jain <aagamjain573@gmail.com>
2023-05-26 15:05:16 +0530

    mobile view navbar

---
+++ src/components/NavBar/MobileNav/index.js
@@ -0,0 +1,91 @@
+import React, { useRef } from "react";
+
+import { List } from "phosphor-react";
+import { NavLink } from "react-router-dom";
+import PropTypes from "prop-types";
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  HStack,
+  Hide,
+  Link,
+  VStack,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+function MobileNav({ links, ...props }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = useRef();
+
+  return (
+    <Hide above="lg">
+      {/* Menu button */}
+      <Button
+        variant="circle"
+        px="0"
+        colorScheme="whiteAlpha"
+        color="white"
+        onClick={onOpen}
+      >
+        <List size={28} weight="bold" />
+      </Button>
+      {/* Menu overlay */}
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+        {...props}
+      >
+        <DrawerOverlay />
+        <DrawerContent bg="gray.900">
+          <DrawerHeader>
+            <HStack justify="end" align="center">
+              <DrawerCloseButton position="initial" color="white" />
+            </HStack>
+          </DrawerHeader>
+          <DrawerBody>
+            {/* Navigation links */}
+            <VStack>
+              {links.map((link) => {
+                return (
+                  <Link
+                    variant="menu"
+                    as={NavLink}
+                    key={`mobile-nav${link.path}`}
+                    to={link.path}
+                  >
+                    {link.name}
+                  </Link>
+                );
+              })}
+            </VStack>
+            {/* Login buttons */}
+            <VStack w="100%" alignItems="stretch" py="3rem">
+              <Button
+                as={NavLink}
+                to="/login"
+                variant="ghost"
+                colorScheme="whiteAlpha"
+                color="white"
+              >
+                Admin Login
+              </Button>
+            </VStack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </Hide>
+  );
+}
+
+MobileNav.propTypes = {
+  links: PropTypes.array,
+};
+
+export default MobileNav;
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+
+import { NavLink } from "react-router-dom";
+import { Box, Button, Container, HStack, Link, Show } from "@chakra-ui/react";
+
+import LogoWhite from "assets/images/brand-logo/logo.svg";
+import MobileNav from "./MobileNav";
+
+function NavBar() {
+  const [navLinks] = useState([
+    {
+      name: "CP Contest Calendar",
+      path: "/cpcontestcalendar",
+    },
+    {
+      name: "Vibrant Color Tools",
+      path: "/vibrantcolortools",
+    },
+    {
+      name: "Site Saver",
+      path: "/sitesaver",
+    },
+  ]);
+
+  return (
+    <Box bg="gray.900">
+      <Container maxW="container.xl" py="1.5rem">
+        <HStack justifyContent="space-between" spacing="6rem">
+          {/* Left links */}
+          <HStack spacing="6rem">
+            <Link as={NavLink} to="/">
+              <LogoWhite />
+            </Link>
+            <Show above="lg">
+              <HStack spacing="5.25rem">
+                {navLinks.map((link) => {
+                  return (
+                    <Button
+                      key={`nav${link.path}`}
+                      as={NavLink}
+                      to={link.path}
+                      variant="link"
+                      colorScheme="whiteAlpha"
+                      color="white"
+                    >
+                      {link.name}
+                    </Button>
+                  );
+                })}
+              </HStack>
+            </Show>
+          </HStack>
+          {/* Right Links */}
+          <Show above="lg">
+            <HStack spacing="1.125rem">
+              <Button as={NavLink} to="/signup" colorScheme="blue">
+                Admin Login
+              </Button>
+            </HStack>
+          </Show>
+          {/* Mobile menu */}
+          <MobileNav links={navLinks} />
+        </HStack>
+      </Container>
+    </Box>
+  );
+}
+
+export default NavBar;
