f8d452b972c7e50d1b810ff7bff088e2d8e45146
Aagam Jain <79409258+getlost01@users.noreply.github.com>
2023-06-24 12:56:52 +0530

    clean up commit
    - code formatting
    - add comments

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,6 +4,8 @@ import FeaturesBig from "components/FeatureImages";
 import FeaturesGrid from "components/FeatureGrid";
 import Hero from "components/Hero";
 
+// This is the component for the feature page of the extension
+
 function Home(props) {
 
   return (
